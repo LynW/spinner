2aecab33d0dd2fd1d3e6e87b4248a9fbd8968a5c
lynw <carolyn.wang94@gmail.com>
2022-04-22 16:57:44 -0400

    refactored spinner

--- spinner1.js
+++ spinner1.js
@@ -1,38 +1,10 @@
-process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let spinner = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
 
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+let delay = 100;
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   '); 
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   '); 
-}, 110);
-
-setTimeout(() => {
-  process.stdout.write('\r-   '); 
-}, 1300);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   '); 
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write('\r|   '); 
-}, 1700);
+for (let lines in spinner) {
+  setTimeout(() => {
+    process.stdout.write(`\r${lines}   `);
+  }, delay);
+  delay += 200;
+};
